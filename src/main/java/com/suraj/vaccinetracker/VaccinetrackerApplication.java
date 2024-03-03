0faec0491001f651b85ddbcbcb24c12d770c9435
Suraj Sharma <surajsharma286@gmail.com>
2024-03-03 12:33:43 +0530

    Intial Commit

---
+++ src/main/java/com/suraj/vaccinetracker/VaccinetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.vaccinetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinetrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinetrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suraj/vaccinetracker/VaccinetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.vaccinetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinetrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
